b14995b273c5b30defe60875315bb5c7e58d7bbe
- <sandeepkumarhari@dxc.com>
2022-02-04 00:44:11 +0530

    Version 2

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
-const express=require('express');
-const app =express();
-const bodyParser=require('body-parser');
-const res = require('express/lib/response');
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static('public'))
-app.set('view engine', 'ejs');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+var _ = require('lodash');
+const res = require("express/lib/response");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-const postItems=[];
+const postItems = [];
 // Home Page Server
-app.get("/",(req,res)=>{
- res.render("home",{page:"homepage",items:postItems});
-})
+app.get("/", (req, res) => {
+  res.render("home", { page: "homepage", items: postItems });
+});
 
 // Add new Post page
 
-app.get("/newpost",(req,res)=>{
- res.render("addpost",{page:"newpost"})
+app.get("/newpost", (req, res) => {
+  res.render("addpost");
+  console.log(req.params.post);
+});
+
+app.post("/newpost", (req, res) => {
+  const post = {
+    title: req.body.title,
+    content: req.body.postText,
+  };
+  postItems.push(post);
+  res.redirect("/");
+  console.log(postItems);
+});
+
+app.get("/:postTitle",(req,res)=>{
+ const postRequest=_.lowerCase(req.params.postTitle);
+console.log(postRequest);
+ postItems.find((post)=>{
+  if(_.lowerCase(post.title)===postRequest){
+   res.render("post",{
+    title:post.title,
+    content:post.content
+   })
+  }
+
+
+ });
 
 })
 
-app.post("/newpost",(req,res)=>{
- postItems.push(req.body.postText)
- res.redirect("/")
- console.log(postItems)
-})
-
-app.listen(3000,()=>{
- console.log("Server is listening to port 3000")
-})
+app.listen(3000, () => {
+  console.log("Server is listening to port 3000");
+});
